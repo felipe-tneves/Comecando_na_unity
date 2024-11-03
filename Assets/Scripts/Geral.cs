9db85ef7a45265a400a512598d1c6c15a95b4052
felipe-tneves <felipe.tadeusantos1@gmail.com>
2024-11-03 17:52:45 -0300

    feat: Primeiras aulas e Estrutura Condicional

---
+++ Assets/Scripts/Geral.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Geral : MonoBehaviour
+{
+    /*
+        Operadores relacionais
+        
+        == (igual)
+        != (diferente)
+        >  (Maior que)
+        <  (Menor que)
+        <= (Menor igual)
+        >= (Maior igual)
+
+        Operadores Lógicos
+
+        && (E/ AND)
+        || (OU / OR)
+        ! (NÃO / NOT)
+     
+        Variáveis
+    
+        int vidaPlayer = 10;
+        float velocidadePlayer = 50.2f;
+        bool mortePlayer = true;
+        string nomePlayer = "Tadola";
+        char apelido = 'A';
+    */
+
+    [SerializeField] int vidaHeroi = 100;
+    [SerializeField] int vidaVilao = 300;
+    string resultado;
+
+    void Start()
+    {
+        print(vidaVilao == vidaHeroi);//false
+        print(vidaHeroi <= vidaVilao);//true
+        print(vidaVilao != vidaHeroi);//true
+
+        //Operador ternário
+        //codição ? valor a ser atribuido caso verdadeiro : caso falso
+        //resultado = vidaHeroi < vidaVilao ? "Vida Heroi Menor" : "Vida Heroi Maior";
+
+        //print(resultado);
+
+        //Estrutura condicional
+        if(vidaHeroi < vidaVilao)
+        {
+            resultado = "Vida Heroi Menor";
+            print(resultado);
+
+        }
+        else if(vidaHeroi == vidaVilao)
+        {
+            resultado = "Vida Heroi igual a vida Vilao";
+            print(resultado);
+
+        }
+        else
+        {
+            resultado = "Vida Heroi Maior";
+            print(resultado);
+        }
+
+    }
+
+    void Update()
+    {
+       
+    }
+
+}
